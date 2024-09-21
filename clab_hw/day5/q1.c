094f9e2ea19f2f6524a25efea20941b6c28688fa
Srittik Sujan <sritiksujan.gg@gmail.com>
2024-09-22 03:35:24 +0530

    updation

--- clab_hw/day5/q1.c
+++ clab_hw/day5/q1.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-int main() {
+int main()
+{
 
- int a;
- 
- printf("Enter a number:\n");
- scanf("%d", &a);
- 
- if(a%2==0) {
-  printf("%d is an even number.\n", a);
- }
- 
- else {
-  printf("%d is an odd number.\n", a);
- }
- 
-return 0;
+    int a;
+
+    printf("Enter a number:\n");
+    scanf("%d", &a);
+
+    if (a % 2 == 0)
+    {
+        printf("%d is an even number.\n", a);
+    }
+
+    else
+    {
+        printf("%d is an odd number.\n", a);
+    }
+
+    return 0;
 }
--- clab_hw/day5/q2.c
+++ clab_hw/day5/q2.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-int main() {
+int main()
+{
 
-  int a,b,c,d,e;
- 
+  int a, b, c, d, e;
+
   printf("Enter your total marks:\n");
   scanf("%d", &a);
   printf("Enter your total marks obtained in maths and physics:\n");
@@ -13,16 +14,19 @@ int main() {
   scanf("%d", &d);
   printf("Enter your marks in chemistry:\n");
   scanf("%d", &e);
-  
-  if(a>=190 || b>=140) {
-  
-    if(c>=65 && d>=55 && e>=50) {
-     printf("The candidate is eligible for admissions.\n");
+
+  if (a >= 190 || b >= 140)
+  {
+
+    if (c >= 65 && d >= 55 && e >= 50)
+    {
+      printf("The candidate is eligible for admissions.\n");
     }
   }
-  else {
+  else
+  {
     printf("The candidate is not eligible for admissions.\n");
   }
-    
-return 0;
+
+  return 0;
 }
