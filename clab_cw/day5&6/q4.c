50b5bef90e8a7f1862b1f0b817432dcad02f07a5
Srittik Sujan <sritiksujan.gg@gmail.com>
2024-09-22 23:29:10 +0530

    updated leap year code

--- clab_cw/day5&6/q4.c
+++ clab_cw/day5&6/q4.c
@@ -7,11 +7,19 @@ int main()
     printf("Enter the year: \n");
     scanf("%d", &yr);
 
-    if (yr % 4 == 0 || yr % 400 == 0)
+    if (yr % 400 == 0)
     {
         printf("%d is a leap year\n", yr);
     }
-    else
+    else if (yr % 100 == 0 && yr % 4 == 0 && yr % 400 != 0)
+    {
+        printf("%d is not a leap year.\n", yr);
+    }
+    else if (yr % 4 == 0 && yr % 100 != 0 && yr % 400 != 0)
+    {
+        printf("%d is a leap year.\n", yr);
+    }
+    else if ( yr % 4 != 0)
     {
         printf("%d is not a leap year\n", yr);
     }
--- clab_cw/day8/q6.c
+++ clab_cw/day8/q6.c
@@ -2,16 +2,26 @@
 int main()
 {
 
-    int n, pdt;
+    int n,i,pdt;
     pdt = 1;
 
     printf("Enter a num:\n");
     scanf("%d", &n);
 
+    /*
+    i = n;
+    while (i >= 1) {
+        pdt = pdt * i;
+        i--;
+    }
+    */
+    
+    
     for (int i = n; i >= 1; i--)
     {
         pdt = pdt * i;
     }
+    
 
     printf("The factorial is: %d\n", pdt);
 
