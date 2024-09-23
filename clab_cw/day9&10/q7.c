b2e20d7b4d33aab7cd9278936b82f16ac5986cd5
Srittik Sujan <sritiksujan.gg@gmail.com>
2024-09-23 10:31:48 +0530

    updating with day 9

---
+++ clab_cw/day9&10/q7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n, column;
+    
+    printf("Enter the no. of rows:\n");
+    scanf("%d", &n);
+
+    column = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= column; j++)
+        {
+            printf(" * ");
+        }
+        column++;
+        printf("\n");
+    }
+
+    return 0;
+}
